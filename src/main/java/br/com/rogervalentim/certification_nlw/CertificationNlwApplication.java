27927fe529dcb7076eb1d4e79a2b5952b85e1726
rogervalentim <valentim.roger33@gmail.com>
2024-02-12 19:45:13 -0300

    chore: setup java project

---
+++ src/main/java/br/com/rogervalentim/certification_nlw/CertificationNlwApplication.java
@@ -0,0 +1,13 @@
+package br.com.rogervalentim.certification_nlw;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class CertificationNlwApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(CertificationNlwApplication.class, args);
+	}
+
+}
