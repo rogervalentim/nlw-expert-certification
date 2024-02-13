d518818ccc9790f1436f11b75da78c625f113ad7
rogervalentim <valentim.roger33@gmail.com>
2024-02-13 18:39:30 -0300

    feat: add initial AnswersCerificationEntity

---
+++ src/main/java/br/com/rogervalentim/certification_nlw/modules/students/entities/AnswersCertificationEntity.java
@@ -0,0 +1,19 @@
+package br.com.rogervalentim.certification_nlw.modules.students.entities;
+
+import java.util.UUID;
+
+import lombok.AllArgsConstructor;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+@Data
+@AllArgsConstructor
+@NoArgsConstructor
+public class AnswersCertificationEntity {
+    
+    private UUID id;
+    private UUID certificationID;
+    private UUID questionID;
+    private UUID answerID;
+    private boolean isCorrect;
+}
