481c5f649bd6cfd4ebdb8cd35b26284b70fa77a2
rogervalentim <valentim.roger33@gmail.com>
2024-02-14 16:31:16 -0300

    feat: add new verification to VerifyIfHasCertificationUseCase

--- src/main/java/br/com/rogervalentim/certification_nlw/modules/students/useCases/VerifyIfHasCertificationUseCase.java
+++ src/main/java/br/com/rogervalentim/certification_nlw/modules/students/useCases/VerifyIfHasCertificationUseCase.java
@@ -1,16 +1,22 @@
 package br.com.rogervalentim.certification_nlw.modules.students.useCases;
 
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 
 import br.com.rogervalentim.certification_nlw.modules.students.dto.VerifyHasCertificationDTO;
+import br.com.rogervalentim.certification_nlw.modules.students.repositories.CertificationStudentRepository;
 
 @Service
 public class VerifyIfHasCertificationUseCase {
-    
-    public boolean execute( VerifyHasCertificationDTO dto ) {
-        if(dto.getEmail().equals("valentim.roger33@gmail.com") && dto.getTechnology().equals("JAVA")) {
+
+    @Autowired
+    private CertificationStudentRepository certificationStudentRepository;
+
+    public boolean execute(VerifyHasCertificationDTO dto) {
+        var result =this.certificationStudentRepository.findByStudentEmailAndTechnology(dto.getEmail(), dto.getTechnology());
+        if (!result.isEmpty()) {
             return true;
         }
-           return false;
+        return false;
     }
 }
